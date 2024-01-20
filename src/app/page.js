80cc43fb4d22aadb8f822482bf1ed759b6603acd
vinit agarwal <vinitagarwal.garg@gmail.com>
2024-01-21 03:18:49 +0530

    css mistakes

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default function Home({ Component, pageProps }) {
   const [splash,setSplash] = useState(true)
   setTimeout(()=>{
     setSplash(false)
-  },[2000])
+  },[3000])
   return (
     <>
       <Splash />
--- src/app/pages/work/components/Card.js
+++
@@ -1,5 +0,0 @@
-import styles from './card.module.css'
-export default function Card(){
-    return <>
-    </>
-}
